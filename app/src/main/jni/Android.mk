be8bf136558b9f1a35cdcf6f77251873909ec9f0
Lingshan Yang <31738013+crownyoung0303@users.noreply.github.com>
2017-10-29 19:27:23 -0400

    clean

--- app/src/main/jni/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= VideoConvert.cpp
-
-LOCAL_MODULE:= VideoConvert
-
-include $(BUILD_SHARED_LIBRARY)
--- app/src/main/obj/local/armeabi-v7a/objs/VideoConvert/VideoConvert.o.d
+++
@@ -1,2 +0,0 @@
-/Users/Lily/Downloads/ARCamera-master-2/app/src/main/obj/local/armeabi-v7a/objs/VideoConvert/VideoConvert.o: \
-  /Users/Lily/Downloads/ARCamera-master-2/app/src/main/jni/VideoConvert.cpp
